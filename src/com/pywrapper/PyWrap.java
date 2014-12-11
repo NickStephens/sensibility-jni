7d9c4e3ed238f8f00c607ecea3e6d00cf6e8b6d7
Nick Stephens <stenic05@evergreen.edu>
2014-12-11 02:04:14 -0800

    Remove sensor code from PyWrap.java, it is no longer needed

--- src/com/pywrapper/PyWrap.java
+++ src/com/pywrapper/PyWrap.java
@@ -14,71 +14,20 @@ import android.util.Log;
 public class PyWrap extends Activity
 {
     public static String TAG = "PyWrap";
-    public static float globalEvent;
-    private static SensorManager sensorService = null;
-    private Sensor sensor;
-
-    /*
-    private SensorEventListener mySensorEventListener = new SensorEventListener () {
-
-      @Override
-      public void onAccuracyChanged(Sensor sensor, int accuracy)
-      {
-
-      }
-
-      @Override
-      public void onSensorChanged(SensorEvent event)
-      {
-        globalEvent = event.values[0];
-        Log.v(TAG, "Java reporting:" + globalEvent);
-        Log.v(TAG, "x:" + Float.toString(event.values[0])); 
-        Log.v(TAG, "y:" + Float.toString(event.values[1])); 
-        Log.v(TAG, "z:" + Float.toString(event.values[2])); 
-      }
-    };
-    */
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        //Hardware.context = (Context) this;
 
         Log.v(TAG, "Starting up...");
-        /*
-        Log.v(TAG, "Initializing Sensor Manager...");
-        sensorService = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorService.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (sensor != null)
-        {
-          sensorService.registerListener(mySensorEventListener, sensor,
-            SensorManager.SENSOR_DELAY_NORMAL);
-          Log.v(TAG, "Registered accelerometer listener");
-        }
-        */ 
 
         TextView tv = new TextView(this);
         execPyScript("/data/local/tmp/test.py");
         tv.setText("Executed");
         setContentView(tv);
 
-        /* 
-        Hardware.accelerometerEnable(true);
-        while (true)
-        {
-          if (Hardware.lastEvent != null)  
-            Log.v(TAG, Hardware.lastEvent.toString());
-          else
-            Log.v(TAG, "No data to report");  
-          try {
-            Thread.sleep(1000);
-          } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
-          }
-        }
-        */
     }
 
     public native void execPyScript(String script);
